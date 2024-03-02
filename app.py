5eeb4ef984689420b7a7e51cf0afe414663e71b1
Tikam12 <bt21cse031@nituk.ac.in>
2024-03-02 13:36:18 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+
+# import the model
+try:
+    df = pd.read_pickle('df.pkl')
+except Exception as e:
+    print("Error:", e)
+
+try:
+    pipe = pd.read_pickle('pipe.pkl')
+except Exception as e:
+    print("Error:", e)
+
+# pipe = pickle.load(open('pipe.pkl', 'rb'))
+
+
+
+st.title("Laptop Predictor")
+
+# brand
+company = st.selectbox('Brand',df['Company'].unique())
+
+# type of laptop
+Type = st.selectbox('Type',df['TypeName'].unique())
+
+# ram
+ram = st.selectbox('Ram(in GB)',[2,4,6,8,12,16,24,32,64])
+
+# weight
+weight = st.number_input('weight of the laptop')
+
+# touchscreen
+touchscreen = st.selectbox('Touchscreen',['No','Yes'])
+
+# IPS
+ips = st.selectbox('IPS',['No','Yes'])
+
+# screen size
+screen_size = st.number_input('Screen Size')
+
+# resolution
+resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1440','2304x1440'])
+
+# cpu
+cpu = st.selectbox('CPU',df['Cpu brand'].unique())
+
+hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
+
+ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
+
+gpu = st.selectbox('GPU',df['Gpu brand'].unique())
+
+os = st.selectbox('OS',df['os'].unique())
+
+if st.button('Predict Price'):
+    ppi = None
+    if touchscreen == 'Yes':
+        touchscreen = 1
+    else:
+        touchscreen = 0
+
+    if ips == 'Yes':
+        ips = 1
+    else:
+        ips = 0
+
+    X_res = int(resolution.split('x')[0])
+    Y_res = int(resolution.split('x')[1])
+    ppi = ((X_res ** 2) + (Y_res ** 2)) ** 0.5 / screen_size
+    query = np.array([company, Type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os])
+
+    query = query.reshape(1, 12)
+    st.title("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))))
+
+
